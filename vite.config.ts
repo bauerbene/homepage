002b9322cd61a45bcd53d4299309d4d3162cdf73
Benedikt Bauer <benedikt.bauer@posteo.de>
2025-08-18 00:01:07 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,65 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // automatically update service worker
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "document" ||
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "static-resources",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: /^\/.*$/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "html-pages",
+              networkTimeoutSeconds: 3,
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+              },
+            },
+          },
+        ],
+      },
+      includeAssets: ["offline.html", "mainfest.json"], // static files to cache
+      manifest: {
+        name: "Imposter",
+        orientation: "portrait",
+        short_name: "Imposter",
+        start_url: "/homepage",
+        lang: "de",
+        display: "standalone",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/homepage",
 });
